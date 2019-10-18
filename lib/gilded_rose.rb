04b930266248843b3c76489d5e89122b28414da8
Pengiran Nazrin <pengirannazrin@Pengirans-MacBook-Pro.local>
2019-10-18 16:26:19 +0800

    created array of item names, used && on if statements to reduce the number of lines

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,44 +1,40 @@
 class GildedRose
+  # 1. Created an array to hold the names
+  @@names = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+
   def initialize(items)
     @items = items
   end
-
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != @@names[0] && item.name != @@names[1]
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != @@names[2]
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+          if item.name == @@names[1]
+            # 2. Used && to conjoin the if statments
+            if item.sell_in < 11 && item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 6 && item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if item.name != @@names[2]
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+        if item.name != @@names[0]
+          # 3. Used && to conjoin the if statements
+          if item.name != @@names[1] && item.quality > 0 && item.name != @@names[2]
+            item.quality = item.quality - 1
           else
             item.quality = item.quality - item.quality
           end
